5cd40b6685fab49676abac9e3a27c253d98de7c0
raul <albert@mailinator.com>
2020-02-03 20:11:36 -0600

    Initial Commit

---
+++ Test_RATC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test_RATC
+//
+//  Created by Raúl Torres on 03/02/20.
+//  Copyright © 2020 Raúl Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
